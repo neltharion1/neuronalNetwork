6828f252e4c135d76f21de1f064e299f54a84e38
ing873 <ing873@LWIP02>
2020-01-08 12:18:52 +0100

    neuronalNetwork_v0.0.2

--- NeuronalNetwork/Form1.Designer.cs
+++ NeuronalNetwork/Form1.Designer.cs
@@ -38,6 +38,11 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.Column1_input = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2_hidden = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3_output = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4_inout = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5_out = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
@@ -47,16 +52,22 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1_input,
+            this.Column2_hidden,
+            this.Column3_output,
+            this.Column4_inout,
+            this.Column5_out});
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(882, 594);
+            this.dataGridView1.Size = new System.Drawing.Size(570, 530);
             this.dataGridView1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(927, 22);
+            this.label1.Location = new System.Drawing.Point(628, 20);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(90, 20);
             this.label1.TabIndex = 1;
@@ -66,7 +77,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(927, 48);
+            this.label2.Location = new System.Drawing.Point(628, 46);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(103, 20);
             this.label2.TabIndex = 2;
@@ -76,7 +87,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(927, 74);
+            this.label3.Location = new System.Drawing.Point(628, 72);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(101, 20);
             this.label3.TabIndex = 3;
@@ -84,28 +95,28 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(1083, 22);
+            this.numericUpDown1.Location = new System.Drawing.Point(784, 20);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(68, 20);
             this.numericUpDown1.TabIndex = 4;
             // 
             // numericUpDown2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(1083, 48);
+            this.numericUpDown2.Location = new System.Drawing.Point(784, 46);
             this.numericUpDown2.Name = "numericUpDown2";
             this.numericUpDown2.Size = new System.Drawing.Size(68, 20);
             this.numericUpDown2.TabIndex = 5;
             // 
             // numericUpDown3
             // 
-            this.numericUpDown3.Location = new System.Drawing.Point(1083, 74);
+            this.numericUpDown3.Location = new System.Drawing.Point(784, 72);
             this.numericUpDown3.Name = "numericUpDown3";
             this.numericUpDown3.Size = new System.Drawing.Size(68, 20);
             this.numericUpDown3.TabIndex = 6;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1035, 221);
+            this.button1.Location = new System.Drawing.Point(632, 212);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(116, 47);
             this.button1.TabIndex = 7;
@@ -114,7 +125,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(1035, 152);
+            this.button2.Location = new System.Drawing.Point(632, 143);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(116, 47);
             this.button2.TabIndex = 8;
@@ -123,18 +134,43 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(1035, 293);
+            this.button3.Location = new System.Drawing.Point(632, 284);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(116, 47);
             this.button3.TabIndex = 9;
             this.button3.Text = "Query Network";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // Column1_input
+            // 
+            this.Column1_input.HeaderText = "Input Layer";
+            this.Column1_input.Name = "Column1_input";
+            // 
+            // Column2_hidden
+            // 
+            this.Column2_hidden.HeaderText = "Input Hidden";
+            this.Column2_hidden.Name = "Column2_hidden";
+            // 
+            // Column3_output
+            // 
+            this.Column3_output.HeaderText = "Output Hidden";
+            this.Column3_output.Name = "Column3_output";
+            // 
+            // Column4_inout
+            // 
+            this.Column4_inout.HeaderText = "Input output";
+            this.Column4_inout.Name = "Column4_inout";
+            // 
+            // Column5_out
+            // 
+            this.Column5_out.HeaderText = "Output Layer";
+            this.Column5_out.Name = "Column5_out";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1183, 618);
+            this.ClientSize = new System.Drawing.Size(862, 564);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -168,6 +204,11 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1_input;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2_hidden;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3_output;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4_inout;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5_out;
     }
 }
 
---
+++ NeuronalNetwork/nn3S.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuronalNetwork
+{
+    class nn3S
+    {
+        private double[] final_inputs;
+        private double[] final_outputs;
+        private double[] hidden_inputs;
+        private double[] hidden_outputs;
+        private int hnodes, innodes, onodes;
+        private double[][] who, wih;
+
+        public nn3S(int innodes, int hnodes, int onodes)
+        {
+            this.innodes = innodes;
+            this.hnodes = hnodes;
+            this.onodes = onodes;
+        }
+
+
+    }
+}
